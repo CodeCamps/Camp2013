e7bc6147a52405de61c130601c7b915e82d8ff85
groundh0g <groundh0g@codetopia.com>
2014-04-22 22:32:26 -0500

    Rebuilding history from ZIP files on VM

--- TimGumchewer/TimGumchewer/TimGumchewer/GameScreen.cs
+++ TimGumchewer/TimGumchewer/TimGumchewer/GameScreen.cs
@@ -12,48 +12,102 @@ namespace TimGumchewer
     {
         Texture2D texNormal;
         Texture2D texVolcano;
+        Texture2D texObsticalGum;
+        Texture2D texObsticalSawTop;
+        Texture2D texObsticalSpikes;
+        Texture2D texObsticalFire;
         ContentManager Content;
+
+        Dictionary<PlayerIndex, Player> players =
+            new Dictionary<PlayerIndex, Player>();
 
         public GameScreen(ContentManager content)
         {
             this.Content = content;
             this.texNormal = content.Load<Texture2D>("normal");
-            this.texVolcano = content.Load<Texture2D>("volcano");
+            this.texObsticalGum = content.Load<Texture2D>("mouse");
+            this.texVolcano = content.Load<Texture2D>("cave");
+            this.texObsticalSawTop = content.Load<Texture2D>("sawTop");
+            this.texObsticalSpikes = content.Load<Texture2D>("spikes");
+            this.texObsticalFire = content.Load<Texture2D>("fire");
+
+            var player = new Player();
+            player.PlayerIndex = PlayerIndex.One;
+            player.Reset(1234567);
+
+            players.Add(PlayerIndex.One, player);
+
+            player = new Player();
+            player.PlayerIndex = PlayerIndex.Two;
+            player.Reset(1234567);
+
+            players.Add(PlayerIndex.Two, player);
         }
-
-
-
-        float startX = -96;
 
         public override void Update(GameTime gameTime)
         {
-            startX -= 3;
-            if (startX < -96 * 2)
-            {
-                startX += 96;
-            }
+            var player = players[PlayerIndex.One];
+            player.Move(player.Speed);
+
+            player = players[PlayerIndex.Two];
+            player.Move(player.Speed);
         }
+
+        Rectangle rectScreen = Rectangle.Empty;
 
         public override void Draw(GameTime gameTime, SpriteBatch batch)
         {
             batch.Begin();
 
-            Rectangle rect = new Rectangle(
-                0, 0,
-                batch.GraphicsDevice.Viewport.Width,
-                batch.GraphicsDevice.Viewport.Height);
+            if (rectScreen == Rectangle.Empty)
+            {
+                var viewport = batch.GraphicsDevice.Viewport;
+                rectScreen = new Rectangle(0, 0, viewport.Width, viewport.Height);
+            }
 
-            batch.Draw(texVolcano, rect, Color.White);
+            batch.Draw(texVolcano, rectScreen, Color.White);
 
             var loc = Vector2.Zero;
 
-            loc.Y = batch.GraphicsDevice.Viewport.Height - 128;
-            loc.X = startX;
-
-            for (int i = 0; i < 10; i++)
+            foreach (var playerIndex in players.Keys)
             {
-                batch.Draw(texNormal, loc, Color.White);
-                loc.X += 96;
+                var player = players[playerIndex];
+
+                loc.X = player.startX;
+                if (playerIndex == PlayerIndex.One)
+                {
+                    loc.Y = rectScreen.Height / 2 - 128;
+                }
+                else
+                {
+                    loc.Y = rectScreen.Height - 128;
+                }
+
+                for (int i = 0; i < 11; i++)
+                {
+                    var tile = player.tiles[i];
+                    
+                    Texture2D texture = texNormal;
+                    if (tile.TileType == TileType.GUM)
+                    {
+                        texture = texObsticalGum;
+                    }
+                    else if (tile.TileType == TileType.SAW_TOP)
+                    {
+                        texture = texObsticalSawTop;
+                    }
+                    else if (tile.TileType == TileType.FIRE)
+                    {
+                        texture = texObsticalFire;
+                    }
+                    else if (tile.TileType == TileType.SPIKE)
+                    {
+                        texture = texObsticalSpikes;
+                    }
+
+                    batch.Draw(texture, loc, Color.White);
+                    loc.X += 96;
+                }
             }
 
             batch.End();
--- TimGumchewer/TimGumchewer/TimGumchewer/Player.cs
+++ TimGumchewer/TimGumchewer/TimGumchewer/Player.cs
@@ -15,15 +15,94 @@ namespace TimGumchewer
         public int Health; // 0-3
         public float SpeedBonus; // can be negative
         public int CandiesCollected;
+        public float startX;
 
-        public void Reset()
+        public void Reset(int randomSeed)
         {
             this.Location = 0.0f;
-            this.Speed = 0.0f;
+            this.Speed = 3.0f;
             this.PlayerStatus = TimGumchewer.PlayerStatus.WAITING;
             this.Health = 3;
             this.SpeedBonus = 0.0f;
             this.CandiesCollected = 0;
+            this.startX = -96;
+            this.rand = new Random(randomSeed);
+            this.tiles.Clear();
+            this.FillTiles();
         }
+
+        public void Move(float distance)
+        {
+            startX -= distance;
+            if (startX < -96 * 2)
+            {
+                tiles.RemoveAt(0);
+                startX += 96;
+            }
+
+            if (tiles.Count < 11)
+            {
+                FillTiles();
+            }
+        }
+
+        public List<Tile> tiles = new List<Tile>();
+        public Random rand = null;
+
+        public void FillTiles()
+        {
+            while (tiles.Count < 11)
+            {
+                AddTiles();
+            }
+        }
+
+        public bool isObstacle = false;
+
+        public void AddTiles()
+        {
+            if (isObstacle)
+            {
+                int value = rand.Next(4);
+                var tile = new Tile();
+
+                if (value == 0)
+                {
+                    tile.TileType = TileType.GUM;
+                    tile.Tactic = Tactic.JUMP_OR_DIVE;
+                }
+                else if (value == 1)
+                {
+                    tile.TileType = TileType.SAW_TOP;
+                    tile.Tactic = Tactic.SLIDE;
+                }
+                else if (value == 2)
+                {
+                    tile.TileType = TileType.SPIKE;
+                    tile.Tactic = Tactic.JUMP_OR_DIVE;
+                }
+                else if (value == 3)
+                {
+                    tile.TileType = TileType.FIRE;
+                    tile.Tactic = Tactic.JUMP_OR_DIVE;
+                }
+
+                tiles.Add(tile);
+            }
+            else
+            {
+                int numTiles = rand.Next(6) + 2;
+                for (int i = 0; i < numTiles; i++)
+                {
+                    var tile = new Tile();
+                    tile.TileType = TileType.NORMAL;
+                    tile.Tactic = Tactic.ANY;
+                    tiles.Add(tile);
+                }
+            }
+
+            isObstacle = !isObstacle;
+        }
+
     }
 }
